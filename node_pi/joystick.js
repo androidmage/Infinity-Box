709a2c940a0b33db8d90a51a1d7ffd55cdc48937
Abdullah <ab2asad@uwaterloo.ca>
2018-11-08 12:57:19 -0500

    Implemented the controller server.

---
+++ node_pi/joystick.js
@@ -0,0 +1,57 @@
+const Gpio = require("onoff").Gpio;
+const io = require("socket.io-client");
+
+var socket = io.connect("http://localhost:3000/", {reconnection:true});
+
+
+var left = new Gpio(4, 'in', 'falling', {debounceTimeout: 10});
+var right = new Gpio(22, 'in', 'falling', {debounceTimeout: 10});
+var up = new Gpio(27, 'in', 'falling', {debounceTimeout: 10});
+var down = new Gpio(17, 'in', 'falling', {debounceTimeout: 10});
+
+console.log("Started:");
+
+left.watch(function(err, value) { 
+  if (err) {
+     console.log("There was an error", err);
+     return;
+  }
+  console.log("LEFT BUTTON PRESSED");
+  socket.emit("left", {});
+});
+
+right.watch(function(err, value) { 
+  if (err) {
+     console.log("There was an error", err);
+     return;
+  }
+  console.log("RIGHT BUTTON PRESSED");
+  socket.emit("right", {});
+});
+
+up.watch(function(err, value) { 
+  if (err) {
+     console.log("There was an error", err);
+     return;
+  }
+  console.log("UP BUTTON PRESSED");
+  socket.emit("up");
+});
+
+down.watch(function(err, value) { 
+  if (err) {
+     console.log("There was an error", err);
+     return;
+  }
+  console.log("DOWN BUTTON PRESSED");
+  socket.emit("down");
+});
+
+process.on('SIGINT', function() {
+  left.unexport();
+  right.unexport();
+  up.unexport();
+  down.unexport();
+  console.log("Closing");
+  process.exit();
+});
